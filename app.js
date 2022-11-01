ffbe86411f1676ddbce48466fd71b58844e8d4ed
momras <mohammra@metropolia.fi>
2022-11-01 14:23:36 +0200

    main

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/cat", (req, res) => {
+  res.send("From this endpoint you can get cats.");
+});
+app.post("/cat", (req, res) => {
+  console.log(req);
+  res.send("From this endpoint you can add more cats.");
+});
+app.put("/");
+app.delete();
+app.get("/user", (req, res) => {
+  res.send("From this endpoint you can get users.");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
